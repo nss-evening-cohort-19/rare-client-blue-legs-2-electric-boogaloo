6e44463160f241fcd739efec47b0afeac2dec556
Trinity Christiana <trinitycterry@gmail.com>
2022-10-25 21:56:59 -0500

    updated with correct things

--- src/views/Authorized.js
+++
@@ -1,8 +0,0 @@
-import { Navigate, Outlet } from "react-router-dom"
-
-export const Authorized = ({ token }) => {
-  if (token) {
-    return <Outlet />
-  }
-  return <Navigate to='/login' replace />
-}

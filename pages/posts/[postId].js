d159654efb73b2aa932d5403200de97dd7ff45a0
Justin Ferwerda <justin.ferwerda@gmail.com>
2022-11-05 13:45:09 -0500

    added router to dependency array

--- pages/posts/[postId].js
+++ pages/posts/[postId].js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import getSinglePost from '../../api/postData';
@@ -10,13 +11,11 @@ export default function SinglePostPage() {
 
   const getThePost = () => {
     getSinglePost(postId).then(setPost);
-    console.warn(post);
   };
 
   useEffect(() => {
     getThePost();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [router]);
 
   return (
     <div>

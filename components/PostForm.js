45a94188a9d923cf193641c7a2e0261636206c7f
Brett Hughes <twofivinclimber@gmail.com>
2022-11-05 10:31:20 -0500

    post form in-progression

--- components/PostForm.js
+++ components/PostForm.js
@@ -1,35 +1,71 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button, Form } from 'react-bootstrap';
+import PropTypes from 'prop-types';
 import Async from 'react-select';
 
-function PostForm() {
+const initialState = {
+  id: null,
+  user_id: '',
+  category_id: null,
+  title: '',
+  image_url: '',
+  content: '',
+  publication_date: '',
+  approved: false,
+};
+
+function PostForm({ obj }) {
+  const [input, setInput] = useState(initialState);
+  const [categories, setCategories] = useState();
+  const [tags, setTags] = useState();
+
+  const getTheContent = () => {
+    if (obj.id) {
+      setInput(obj);
+    }
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setInput((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  useEffect(() => {
+    getTheContent();
+  }, []);
+
   return (
     <>
       <h3>New Post</h3>
       <Form>
         <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
           <Form.Label>Title</Form.Label>
-          <Form.Control type="text" onChange="PLACEHOLDER" placeholder="Title goes here" />
+          <Form.Control type="text" onChange={handleChange} placeholder="Title goes here" name="title" value={input.title} />
+
           <Form.Label>Image URL</Form.Label>
-          <Form.Control type="text" onChange="PLACEHOLDER" placeholder="Enter your Image URL" />
+          <Form.Control type="text" onChange={handleChange} placeholder="Enter your Image URL" name="image_url" value={input.image_url} />
+
           <Form.Label>Article</Form.Label>
-          <Form.Control as="textarea" onChange="PLACEHOLDER" value={{ }} rows={3} />
+          <Form.Control as="textarea" onChange={handleChange} rows={3} name="content" value={input.content} />
+
           <Form.Label>Category</Form.Label>
-          <Async
-            classNamePrefix="select"
-            onChange="PLACEHOLDER"
-            isClearable
-            value={{}}
-            loadOptions={' '}
-          />
-          {['JavaScript', 'Python', 'SQL', 'React'].map((tag) => (
-            <div key={`${tag}FormCheck`} className="mb-3">
+          <Form.Select aria-label="Default select example" name="category" onChange={handleChange}>
+            <option>Select a Category</option>
+            {categories.map((cat) => (
+              <option value={cat}>{cat}</option>
+            ))}
+          </Form.Select>
+          {tags.map((tag) => (
+            <div name="tag" key={`${tag}FormCheck`} className="mb-3">
               <Form.Check
                 type="checkbox"
                 id="default-checkbox"
                 label={tag}
-                onChange="PLACEHOLDER"
-                checked="INPUT PLACEHOLDER"
+                onChange={handleChange}
+                checked={input.tag === tag}
               />
             </div>
           ))}
@@ -39,5 +75,22 @@ function PostForm() {
     </>
   );
 }
+
+PostForm.propTypes = {
+  obj: PropTypes.shape({
+    id: PropTypes.number,
+    user_id: PropTypes.string,
+    category_id: PropTypes.number,
+    title: PropTypes.string,
+    image_url: PropTypes.string,
+    content: PropTypes.string,
+    publication_date: PropTypes.string,
+    approved: PropTypes.bool,
+  }),
+};
+
+PostForm.defaultProps = {
+  obj: initialState,
+};
 
 export default PostForm;

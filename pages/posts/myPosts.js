b0cb865bdc3e708c6e373f665d063255d3258db5
Christian Black <blackl3@gmail.com>
2022-11-07 19:33:01 -0600

    saving changes

--- pages/posts/myPosts.js
+++ pages/posts/myPosts.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getPostsByAuthorId } from '../../api/postData';
 
-export default function myPostsPage() {
+export default function MyPostsPage() {
+  const [posts, setPosts] = useState();
+  const [token, setToken] = useState(null);
+
+  const getMyPosts = () => {
+    getPostsByAuthorId((Number(token))).then(setPosts);
+  };
+
+  useEffect(() => {
+    const userToken = localStorage.getItem('auth_token');
+    setToken(userToken);
+    getMyPosts();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [token]);
+
   return (
-    <div>My Posts Page</div>
+    <>
+      <div>My Posts Page</div>
+      {posts?.map((post) => post.title)}
+    </>
   );
 }

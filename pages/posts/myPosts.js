541e5cb61737bd4c6c371f7490686b9daba2c71a
Christian Black <blackl3@gmail.com>
2022-11-07 21:20:11 -0600

    saving changes

--- pages/posts/myPosts.js
+++ pages/posts/myPosts.js
@@ -1,9 +1,12 @@
+import { useRouter } from 'next/router';
 import React, { useEffect, useState } from 'react';
 import { getPostsByAuthorId } from '../../api/postData';
+import PostCard from '../../components/PostCard';
 
 export default function MyPostsPage() {
   const [posts, setPosts] = useState();
   const [token, setToken] = useState(null);
+  const router = useRouter();
 
   const getMyPosts = () => {
     getPostsByAuthorId((Number(token))).then(setPosts);
@@ -19,7 +22,9 @@ export default function MyPostsPage() {
   return (
     <>
       <div>My Posts Page</div>
-      {posts?.map((post) => post.title)}
+      {posts?.map((postObj) => (
+        <PostCard key={postObj.id} router={router.asPath} postObject={postObj} onUpdate={getMyPosts} />
+      ))}
     </>
   );
 }

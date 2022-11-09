9a91972e286c68c88ac3d627e730f45f4e9e3b1d
Lesley Keyes <lesleyjkeyes@gmail.com>
2022-11-08 21:28:54 -0600

    started category filter

--- pages/posts/category/[category_id].js
+++ pages/posts/category/[category_id].js
@@ -1,7 +1,27 @@
-import React from 'react';
+import { useEffect, useState, React } from 'react';
+import { useRouter } from 'next/router';
+import { getPostsByCategory } from '../../../api/categoryData';
+import PostCard from '../../../components/PostCard';
 
 export default function ViewPostsByCategory() {
+  const [posts, setPosts] = useState({});
+  const router = useRouter();
+  const { id } = router.query;
+
+  const getPosts = () => {
+    getPostsByCategory(id).then(setPosts);
+  };
+
+  useEffect(() => {
+    getPosts();
+  }, [router]);
+  console.warn(posts);
+
   return (
-    <div>Post by Category</div>
+    <div>
+      {posts?.map((postObj) => (
+        <PostCard key={postObj.id} router={router.asPath} postObject={postObj} onUpdate={getPosts} />
+      ))}
+    </div>
   );
 }

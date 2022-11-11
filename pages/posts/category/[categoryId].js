66a1f4d3c165e56f3b93500e4764c9aa3f0e6a2f
Lesley Keyes <lesleyjkeyes@gmail.com>
2022-11-10 20:05:12 -0600

    updating post card

--- pages/posts/category/[categoryId].js
+++ pages/posts/category/[categoryId].js
@@ -7,19 +7,22 @@ export default function ViewPostsByCategory() {
   const [posts, setPosts] = useState([]);
   const router = useRouter();
   const { categoryId } = router.query;
+  const [token, setToken] = useState(null);
 
   const getPosts = () => {
     getPostsByCategory(categoryId).then(setPosts);
   };
 
   useEffect(() => {
+    const userToken = localStorage.getItem('auth_token');
+    setToken(userToken);
     getPosts();
   }, [categoryId]);
 
   return (
     <div>
       {posts?.map((postObj) => (
-        <PostCard key={postObj.id} router={router.asPath} postObject={postObj} onUpdate={getPosts} />
+        <PostCard userToken={token} key={postObj.id} postObject={postObj} onUpdate={getPosts} />
       ))}
     </div>
   );

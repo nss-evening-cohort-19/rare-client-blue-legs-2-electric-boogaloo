909f1503294335924f2bf0e7b35773f3dbbdfb01
Christian Black <blackl3@gmail.com>
2022-11-10 20:10:49 -0600

    saving changes

--- components/ProfileCard.js
+++ components/ProfileCard.js
@@ -1,8 +1,17 @@
-import React from 'react';
-import { Card } from 'react-bootstrap';
+import React, { useEffect, useState } from 'react';
+import { Button, Card } from 'react-bootstrap';
 import PropTypes from 'prop-types';
+// import UnsubscribeIcon from '@mui/icons-material/Unsubscribe';
+import PersonAddAltIcon from '@mui/icons-material/PersonAddAlt';
 
 export default function ProfileCard({ obj }) {
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    const userToken = localStorage.getItem('auth_token');
+    setToken(Number(userToken));
+  }, [token]);
+
   return (
     <Card className="user-card">
       <div className="user-card-left">
@@ -22,6 +31,9 @@ export default function ProfileCard({ obj }) {
         <div className="user-bio">
           <Card.Text>{obj.bio}</Card.Text>
         </div>
+        { token !== obj.id ? (
+          <Button><PersonAddAltIcon /></Button>
+        ) : ''}
       </div>
     </Card>
   );
@@ -29,6 +41,7 @@ export default function ProfileCard({ obj }) {
 
 ProfileCard.propTypes = {
   obj: PropTypes.shape({
+    id: PropTypes.number,
     username: PropTypes.string,
     first_name: PropTypes.string,
     last_name: PropTypes.string,
